74b2da1c5e3e65f47eb0136c18fd94cdd0cb2518
aidex-hackability <32888714+aidex-hackability@users.noreply.github.com>
2017-10-30 20:33:06 -0400

    initial

---
+++ schema.js
@@ -0,0 +1,79 @@
+
+{
+  "intents": [
+    {
+      "slots": [
+        {
+          "name": "System",
+          "type": "AIDEX_SYSTEMS"
+        }
+      ],
+      "intent": "SystemIntent"
+    },
+    {
+      "slots": [
+        {
+          "name": "City",
+          "type": "AMAZON.US_CITY"
+        }
+      ],
+      "intent": "ClimateIntent"
+    },
+    {
+      "slots": [
+        {
+          "name": "FanLevel",
+          "type": "AIDEX_FAN_LEVELS"
+        }
+      ],
+      "intent": "FanIntent"
+    },
+    {
+      "slots": [
+        {
+          "name": "PowerSelector",
+          "type": "AIDEX_POWER_SELECTORS"
+        }
+      ],
+      "intent": "PowerIntent"
+    },
+    {
+      "slots": [
+        {
+          "name": "LightLevel",
+          "type": "AIDEX_LIGHT_LEVELS"
+        }
+      ],
+      "intent": "LightIntent"
+    },
+    {
+      "slots": [
+        {
+          "name": "DriveDirection",
+          "type": "AIDEX_DRIVE_DIRECTIONS"
+        },
+        {
+          "name": "DriveDegree",
+          "type": "AIDEX_DRIVE_DEGREES"
+        },
+        {
+          "name": "DriveSpeed",
+          "type": "AIDEX_DRIVE_SPEEDS"         
+        }
+      ],
+      "intent": "DriveIntent"
+    },
+    {
+      "intent": "AMAZON.StopIntent"
+    },
+    {
+      "intent": "AMAZON.CancelIntent"
+    },
+    {
+      "intent": "AMAZON.StartOverIntent"
+    },
+    {
+      "intent": "AMAZON.HelpIntent"
+    }
+  ]
+}
